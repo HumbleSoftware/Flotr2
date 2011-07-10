b36c9d8e045a701a1449c30ae48998e8dd0d6dc1
carl@humblesoftware.com <carl@humblesoftware.com@4daae283-1d46-0410-8e0a-abc221dbbbb9>
2011-07-10 20:21:59 +0000

    Added stopObserving.
    git-svn-id: https://flotr.googlecode.com/svn/branches/flotr2@226 4daae283-1d46-0410-8e0a-abc221dbbbb9

--- js/Flotr.EventAdapter.js
+++ js/Flotr.EventAdapter.js
@@ -9,5 +9,9 @@ Flotr.EventAdapter = {
   fire: function(object, name, args) {
     Event.fire(object, name, args);
     return this;
+  },
+  stopObserving: function(object, name, callback) {
+    Event.stopObserving(object, name, callback);
+    return this;
   }
 };
