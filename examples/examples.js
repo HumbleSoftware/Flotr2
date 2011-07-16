0bef20a4c0ef10e07ae6323a02c63d629bdf7485
carl@humblesoftware.com <carl@humblesoftware.com@4daae283-1d46-0410-8e0a-abc221dbbbb9>
2011-07-16 15:38:29 +0000

    Retabbed and removed prototype dependency.
    git-svn-id: https://flotr.googlecode.com/svn/branches/flotr2@271 4daae283-1d46-0410-8e0a-abc221dbbbb9

--- examples/examples.js
+++ examples/examples.js
@@ -1,24 +1,25 @@
 // Gets the value from a group of radio buttons
 function getV(nl) {
-	var v = null;
-	$A(nl).each(function(e) {
-	  if (e.checked) {
-	    v = e.value;
-	      return;
-	    }
-	});
-	return v;
+  var v = null;
+  _.each(nl, function(e) {
+    if (e.checked) {
+      v = e.value;
+        return;
+      }
+  });
+  return v;
 }
 
 (function(){
-	var view = $('code-view');
-	if (view) {
-		var code = $$('body script')[0].innerHTML.gsub('\n\t\t\t', '\n');
-		if (view.outerHTML) 
-			view.outerHTML = '<pre id="code-view"><code class="javascript">' + code + '</code></pre>';
-		else 
-			view.innerHTML = code;
-	}
-	
-	$$('#wrapper h1')[0].innerHTML = $$('head title')[0].innerHTML;
+  var view = document.getElementById('code-view');
+  if (view) {
+    var code = document.body.getElementsByTagName('script')[0].innerHTML.replace(/\n\t\t\t/g, '\n');
+    if (view.outerHTML) 
+      view.outerHTML = '<pre id="code-view"><code class="javascript">' + code + '</code></pre>';
+    else 
+      view.innerHTML = code;
+  }
+  
+  document.getElementById('wrapper').getElementsByTagName('h1')[0].innerHTML =
+    document.getElementsByTagName('title')[0].innerHTML;
 })();
