a7583e9e1c56b852208d2367576e106e79f7ea94
carl@humblesoftware.com <carl@humblesoftware.com@4daae283-1d46-0410-8e0a-abc221dbbbb9>
2011-07-16 15:35:35 +0000

    Converted adapter to bean.  Added adapter for Prototype for existing applications.
    git-svn-id: https://flotr.googlecode.com/svn/branches/flotr2@242 4daae283-1d46-0410-8e0a-abc221dbbbb9

--- js/Flotr.EventAdapter.js
+++ js/Flotr.EventAdapter.js
@@ -3,15 +3,18 @@
  */
 Flotr.EventAdapter = {
   observe: function(object, name, callback) {
-    Event.observe(object, name, callback);
+    bean.add(object, name, callback);
     return this;
   },
   fire: function(object, name, args) {
-    Event.fire(object, name, args);
+    bean.fire(object, name, args);
+    if (Prototype)
+      Event.fire(object, name, args);
+    // @TODO Someone who uses mootools, add mootools adapter for existing applciations.
     return this;
   },
   stopObserving: function(object, name, callback) {
-    Event.stopObserving(object, name, callback);
+    bean.remove(object, name, callback);
     return this;
   }
 };
