992c82f3dd4a1b5fdd6d9689d78e7ec16f6d658e
Carl Sutherland <carl@humblesoftware.com>
2011-08-06 18:53:06 -0400

    Added getArea method to return a normalization of the selected area.

--- js/plugins/selection.js
+++ js/plugins/selection.js
@@ -57,6 +57,20 @@ Flotr.addPlugin('selection', {
     }
   },
 
+  getArea: function() {
+
+    var s = this.selection.selection,
+      first = s.first,
+      second = s.second;
+
+    return {
+      x1: Math.min(first.x, second.x),
+      x2: Math.max(first.x, second.x),
+      y1: Math.min(first.y, second.y),
+      y2: Math.max(first.y, second.y)
+    };
+  },
+
   selection: {first: {x: -1, y: -1}, second: {x: -1, y: -1}},
   prevSelection: null,
   interval: null,
