fb02c970820766a4edad82d2a4229a965c934795
Carl Sutherland <carl@humblesoftware.com>
2011-08-23 09:50:39 -0400

    Added touch support for eventPointer.

--- js/Flotr.EventAdapter.js
+++ js/Flotr.EventAdapter.js
@@ -18,7 +18,9 @@ Flotr.EventAdapter = {
     return this;
   },
   eventPointer: function(e) {
-    if (Flotr.isIE && Flotr.isIE < 9) {
+    if (!_.isUndefined(e.touches) && e.touches.length > 0) {
+      return {x: e.touches[0].pageX, y: e.touches[0].pageY};
+    } else if (Flotr.isIE && Flotr.isIE < 9) {
       return {x: e.clientX + document.body.scrollLeft, y: e.clientY + document.body.scrollTop};
     } else {
       return {x: e.pageX, y: e.pageY};
