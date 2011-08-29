14e9804b3f1a8eb55fdea68bd92d5d374adbf64e
Carl Sutherland <carl@humblesoftware.com>
2011-08-28 20:32:32 -0400

    Cleanup default tick calculation.

--- js/Flotr.Axis.js
+++ js/Flotr.Axis.js
@@ -138,36 +138,41 @@ Axis.prototype = {
   },
 
   _calculateTicks : function () {
-    // Round to nearest multiple of tick size.
-    var axis = this,
-        o = axis.options,
-        start = axis.tickSize * Math.ceil(axis.min / axis.tickSize),
-        decimals, minorTickSize, v, v2;
+
+    var axis      = this,
+        o         = axis.options,
+        tickSize  = axis.tickSize,
+        min       = axis.min,
+        max       = axis.max,
+        start     = tickSize * Math.ceil(min / tickSize), // Round to nearest multiple of tick size.
+        decimals,
+        minorTickSize,
+        v, v2,
+        i, j;
     
     if (o.minorTickFreq)
-      minorTickSize = axis.tickSize / o.minorTickFreq;
+      minorTickSize = tickSize / o.minorTickFreq;
                       
     // Then store all possible ticks.
-    for(i = 0; start + i * axis.tickSize <= axis.max; ++i){
-      v = v2 = start + i * axis.tickSize;
+    for (i = 0; (v = v2 = start + i * tickSize) <= max; ++i){
       
       // Round (this is always needed to fix numerical instability).
       decimals = o.tickDecimals;
-      if(decimals == null) decimals = 1 - Math.floor(Math.log(axis.tickSize) / Math.LN10);
-      if(decimals < 0) decimals = 0;
+      if (decimals == null) decimals = 1 - Math.floor(Math.log(tickSize) / Math.LN10);
+      if (decimals < 0) decimals = 0;
       
       v = v.toFixed(decimals);
       axis.ticks.push({ v: v, label: o.tickFormatter(v) });
 
       if (o.minorTickFreq) {
-        for(var j = 0; j < o.minorTickFreq && (i * axis.tickSize + j * minorTickSize) < axis.max; ++j) {
-          v = v2 + j * minorTickSize;
-          v = v.toFixed(decimals);
+        for (j = 0; j < o.minorTickFreq && (i * tickSize + j * minorTickSize) < max; ++j) {
+          v = (v2 + j * minorTickSize).toFixed(decimals);
           axis.minorTicks.push({ v: v, label: o.tickFormatter(v) });
         }
       }
     }
   }
+
 };
 
 
