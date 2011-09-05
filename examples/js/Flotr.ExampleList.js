8612cde42dc722da0822a44d882f2832ccfd7f3b
Carl Sutherland <carl@humblesoftware.com>
2011-09-05 16:30:28 -0400

    Added mouse zoom example.

--- examples/js/Flotr.ExampleList.js
+++ examples/js/Flotr.ExampleList.js
@@ -69,6 +69,12 @@ ExampleList.push({
   callback : mouse_tracking
 });
 
+ExampleList.push({
+  key : 'mouse-zoom',
+  name : 'Mouse Zoom',
+  callback : mouse_zoom
+});
+
 
 
 function basic (container) {
@@ -456,6 +462,59 @@ graph = Flotr.draw(
 
 };      
 
+
+function mouse_zoom (container) {
+
+var
+  d1 = [],
+  d2 = [],
+  d3 = [],
+  options,
+  graph,
+  i;
+
+for (i = 0; i < 40; i += 0.5) {
+  d1.push([i, Math.sin(i)+3*Math.cos(i)]);
+  d2.push([i, Math.pow(1.1, i)]);
+  d3.push([i, 40 - i+Math.random()*10]);
+}
+    
+options = {
+  selection : { mode : 'x', fps : 30 }
+};
+  
+// Draw graph with default options, overwriting with passed options
+function drawGraph (opts) {
+
+  // Clone the options, so the 'options' variable always keeps intact.
+  var o = _.extend(_.clone(options), opts || {});
+
+  // Return a new graph.
+  return Flotr.draw(
+    container,
+    [ d1, d2, d3 ],
+    o
+  );
+}
+
+// Actually draw the graph.
+graph = drawGraph();      
+  
+// Hook into the 'flotr:select' event.
+Flotr.EventAdapter.observe(container, 'flotr:select', function (area) {
+
+  // Draw graph with new area
+  f = drawGraph({
+    xaxis: {min:area.x1, max:area.x2},
+    yaxis: {min:area.y1, max:area.y2}
+  });
+
+});
+  
+// When graph is clicked, draw the graph with default area.
+Flotr.EventAdapter.observe(container, 'flotr:click', function () { drawGraph(); });
+
+};      
 
 Flotr.ExampleList =  ExampleList;
 
