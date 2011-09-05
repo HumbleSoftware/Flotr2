9a4e37545663562a3fb858fb9c29495f7e99361b
Carl Sutherland <carl@humblesoftware.com>
2011-09-05 17:27:10 -0400

    Added basic time example.

--- examples/js/Flotr.ExampleList.js
+++ examples/js/Flotr.ExampleList.js
@@ -81,6 +81,11 @@ ExampleList.push({
   callback : mouse_drag
 });
 
+ExampleList.push({
+  key : 'basic-time',
+  name : 'Basic Time',
+  callback : basic_time
+});
 
 function basic (container) {
 
@@ -519,7 +524,6 @@ function mouse_zoom (container) {
     
   // When graph is clicked, draw the graph with default area.
   Flotr.EventAdapter.observe(container, 'flotr:click', function () { drawGraph(); });
-
 };
  
 function mouse_drag (container) {
@@ -542,6 +546,7 @@ function mouse_drag (container) {
   options = {
     xaxis: {min: 0, max: 20}
   };
+
   // Draw graph with default options, overwriting with passed options
   function drawGraph (opts) {
 
@@ -584,6 +589,59 @@ function mouse_drag (container) {
   Flotr.EventAdapter.observe(graph.overlay, 'mousedown', initializeDrag);
 
 };
+
+function basic_time (container) {
+
+  var
+    d1    = [],
+    start = new Date("2009/01/01 01:00").getTime(),
+    options,
+    graph,
+    i, x, o;
+
+  for (i = 0; i < 100; i++) {
+    x = start+(i*1000*3600*24*36.5);
+    d1.push([x, i+Math.random()*30+Math.sin(i/20+Math.random()*2)*20+Math.sin(i/10+Math.random())*10]);
+  }
+        
+  options = {
+    xaxis : {
+      mode : 'time', 
+      labelsAngle : 45
+    },
+    selection : {
+      mode : 'x'
+    },
+    HtmlText : false
+  };
+        
+  // Draw graph with default options, overwriting with passed options
+  function drawGraph (opts) {
+
+    // Clone the options, so the 'options' variable always keeps intact.
+    o = _.extend(_.clone(options), opts || {});
+
+    // Return a new graph.
+    return Flotr.draw(
+      container,
+      [ d1 ],
+      o
+    );
+  }
+
+  graph = drawGraph();      
+        
+  Flotr.EventAdapter.observe(container, 'flotr:select', function(area){
+    // Draw selected area
+    graph = drawGraph({
+      xaxis : { min : area.x1, max : area.x2, mode : 'time', labelsAngle : 45 },
+      yaxis : { min : area.y1, max : area.y2 }
+    });
+  });
+        
+  // When graph is clicked, draw the graph with default area.
+  Flotr.EventAdapter.observe(container, 'flotr:click', function () { graph = drawGraph(); });
+};      
 
 Flotr.ExampleList =  ExampleList;
 
