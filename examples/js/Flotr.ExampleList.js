512cc6e2bbfe421cd9c9ca58b044364f13bb8ed5
Carl Sutherland <carl@humblesoftware.com>
2011-09-05 17:51:25 -0400

    Added click example.

--- examples/js/Flotr.ExampleList.js
+++ examples/js/Flotr.ExampleList.js
@@ -93,6 +93,12 @@ ExampleList.push({
   callback : negative_values
 });
 
+ExampleList.push({
+  key : 'click-example',
+  name : 'Click Example',
+  callback : click_example
+});
+
 function basic (container) {
 
   var
@@ -702,6 +708,38 @@ function negative_values (container) {
     }
   );
 };
+
+function click_example (container) {			
+
+  var
+    d1 = [[0,0]], // Point at origin
+    options,
+    graph;
+
+  options = {
+    xaxis: {min: 0, max: 15},
+    yaxis: {min: 0, max: 15},
+    lines: {show: true},
+    points: {show: true},
+    mouse: {track:true}
+  };
+
+  graph = Flotr.draw(container, [d1], options);
+
+  // Add a point to the series and redraw the graph
+  Flotr.EventAdapter.observe(container, 'flotr:click', function(position){
+
+    // Add a point to the series at the location of the click
+    d1.push([position.x, position.y]);
+    
+    // Sort the series.
+    d1 = d1.sort(function (a, b) { return a[0] - b[0]; });
+    
+    // Redraw the graph, with the new series.
+    graph = Flotr.draw(container, [d1], options);
+  });
+};			
+
 
 Flotr.ExampleList =  ExampleList;
 
