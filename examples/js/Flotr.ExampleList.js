9b176bc2d406960d3966794ecb1de5ac7d817f6b
Carl Sutherland <carl@humblesoftware.com>
2011-09-05 20:11:25 -0400

    Added data download example.

--- examples/js/Flotr.ExampleList.js
+++ examples/js/Flotr.ExampleList.js
@@ -116,6 +116,12 @@ ExampleList.push({
     '</form>'
 });
 
+ExampleList.push({
+  key : 'download-data',
+  name : 'Download Data',
+  callback : download_data
+});
+
 function basic (container) {
 
   var
@@ -821,6 +827,61 @@ function download_image (container) {
   }
 
   return graph;
+};
+
+function download_data (container) {
+
+  var
+    d1 = [],
+    d2 = [],
+    d3 = [],
+    d4 = [],
+    d5 = [],
+    graph,
+    i
+      ;
+  
+  for (i = 0; i <= 10; i += 0.1) {
+    d1.push([i, 4 + Math.pow(i,1.5)]);
+    d2.push([i, Math.pow(i,3)]);
+    d3.push([i, i*5+3*Math.sin(i*4)]);
+    d4.push([i, i]);
+    if( i.toFixed(1)%1 == 0 ){
+      d5.push([i, 2*i]);
+    }
+  }
+          
+  // Draw the graph.
+  graph = Flotr.draw(
+    container, [ 
+      { data : d1, label : 'y = 4 + x^(1.5)', lines : { fill : true } },
+      { data : d2, label : 'y = x^3' },
+      { data : d3, label : 'y = 5x + 3sin(4x)' },
+      { data : d4, label : 'y = x' },
+      { data : d5, label : 'y = 2x', lines : { show : true }, points : { show : true } }
+    ],{
+      xaxis : {
+        noTicks : 7,
+        tickFormatter : function (n) { return '('+n+')'; },
+        min: 1,   // Part of the series is not displayed.
+        max: 7.5
+      },
+      yaxis : {
+        ticks : [[ 0, "Lower"], 10, 20, 30, [40, "Upper"]],
+        max : 40
+      },
+      grid : {
+        verticalLines : false,
+        backgroundColor : 'white'
+      },
+      legend : {
+        position : 'nw'
+      },
+      spreadsheet : {
+        show : true,
+        tickFormatter : function (e) { return e+''; }
+      }
+  });
 };
 
 Flotr.ExampleList =  ExampleList;
