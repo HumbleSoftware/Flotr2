b0a2f7ff2e12d3ae176fa3a7df26145aff48af2f
Carl Sutherland <carl@humblesoftware.com>
2011-09-07 15:29:53 -0400

    Factored calculateRange into Axis.

--- js/Graph.js
+++ js/Graph.js
@@ -134,14 +134,18 @@ Flotr.Graph.prototype = {
       }
     }, this);
 
-    this.calculateRange(a.x, 'x');
+    _.each(a, function (axis) {
+      axis.calculateRange();
+    });
+
+    this.extendRange(a.x, 'x');
     if (a.x2.used) {
-      this.calculateRange(a.x2, 'x');
+      this.extendRange(a.x2, 'x');
     }
 
-    this.calculateRange(a.y, 'y');
+    this.extendRange(a.y, 'y');
     if (a.y2.used) {
-      this.calculateRange(a.y2, 'y');
+      this.extendRange(a.y2, 'y');
     }
   },
 
@@ -168,76 +172,7 @@ Flotr.Graph.prototype = {
       }
     }
   },
-  /**
-   * Calculates the range of an axis to apply autoscaling.
-   * @param {Object} axis - The axis for what the range will be calculated
-   */
-  calculateRange: function(axis, type){
-    var o = axis.options,
-        min = o.min != null ? o.min : axis.datamin,
-        max = o.max != null ? o.max : axis.datamax,
-        margin = o.autoscaleMargin;
-        
-    if (o.scaling == 'logarithmic') {
-      if (min <= 0) min = axis.datamin;
 
-      // Let it widen later on
-      if (max <= 0) max = min;
-    }
-
-    if(max - min == 0.0){
-      var widen = (max == 0.0) ? 1.0 : 0.01;
-      min -= widen;
-      max += widen;
-    }
-
-    if (o.scaling === 'logarithmic') {
-      if (min < 0) min = max / o.base;  // Could be the result of widening
-
-      var maxexp = Math.log(max);
-      if (o.base != Math.E) maxexp /= Math.log(o.base);
-      maxexp = Math.ceil(maxexp);
-
-      var minexp = Math.log(min);
-      if (o.base != Math.E) minexp /= Math.log(o.base);
-      minexp = Math.ceil(minexp);
-      
-      axis.tickSize = Flotr.getTickSize(o.noTicks, minexp, maxexp, o.tickDecimals === null ? 0 : o.tickDecimals);
-                        
-      // Try to determine a suitable amount of miniticks based on the length of a decade
-      if (o.minorTickFreq === null) {
-        if (maxexp - minexp > 10)
-          o.minorTickFreq = 0;
-        else if (maxexp - minexp > 5)
-          o.minorTickFreq = 2;
-        else
-          o.minorTickFreq = 5;
-      }
-    } else {
-      axis.tickSize = Flotr.getTickSize(o.noTicks, min, max, o.tickDecimals);
-    }
-
-    axis.min = min;
-    axis.max = max; //extendRange may use axis.min or axis.max, so it should be set before it is caled
-
-    this.extendRange(axis, type);//extendRange probably changed axis.min and axis.max
-    
-    // Autoscaling. @todo This probably fails with log scale. Find a testcase and fix it
-    if(o.min == null && o.autoscale){
-      axis.min -= axis.tickSize * margin;
-      // Make sure we don't go below zero if all values are positive.
-      if(axis.min < 0 && axis.datamin >= 0) axis.min = 0;
-      axis.min = axis.tickSize * Math.floor(axis.min / axis.tickSize);
-    }
-    
-    if(o.max == null && o.autoscale){
-      axis.max += axis.tickSize * margin;
-      if(axis.max > 0 && axis.datamax <= 0 && axis.datamax != axis.datamin) axis.max = 0;        
-      axis.max = axis.tickSize * Math.ceil(axis.max / axis.tickSize);
-    }
-
-    if (axis.min == axis.max) axis.max = axis.min + 1;
-  },
   /**
    * Calculates axis label sizes.
    */
