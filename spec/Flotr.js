3fe795cc9b096b625cb70a1e04829ca10a581a67
Carl Sutherland <carl@humblesoftware.com>
2011-11-18 16:11:11 -0500

    Added tick size tests.

--- spec/Flotr.js
+++ spec/Flotr.js
@@ -1,51 +1,68 @@
-describe('Plots', function () {
+describe('Flotr', function () {
 
-  var
-    nodeA, nodeB,
-    a, b;
 
-  beforeEach(function () {
+  describe('Plots', function () {
 
-    // Add imagediff matchers
-    this.addMatchers(imagediff.jasmine);
+    var
+      nodeA, nodeB,
+      a, b;
 
-    nodeA = buildNode();
-    nodeB = buildNode();
+    beforeEach(function () {
+
+      // Add imagediff matchers
+      this.addMatchers(imagediff.jasmine);
+
+      nodeA = buildNode();
+      nodeB = buildNode();
+    });
+
+    afterEach(function () {
+      destroyNode(nodeA);
+      destroyNode(nodeB);
+      a = b = null;
+    });
+
+    _.each(TestFlotr.ExampleList.examples, function (example, key) {
+
+      it('should draw a `' + example.name + '`line graph', function () {
+
+        a = executeExampleTest(example, StableFlotr, nodeA);
+        b = executeExampleTest(example, TestFlotr, nodeB);
+
+        expect(b).toImageDiffEqual(a);
+      });
+    });
+
+    function executeExampleTest (example, flotr, node) {
+      Math.seedrandom(example.key);
+      Flotr = flotr;
+      example.callback(node);
+      return node.graph.ctx;
+    }
+
+    function buildNode () {
+      var node = document.createElement('div');
+      document.body.appendChild(node);
+      node.style.width = '320px';
+      node.style.height = '240px';
+      return node;
+    }
+
+    function destroyNode (node) {
+      document.body.removeChild(node);
+    }
   });
 
-  afterEach(function () {
-    destroyNode(nodeA);
-    destroyNode(nodeB);
-    a = b = null;
-  });
+  describe('Main', function () {
 
-  _.each(TestFlotr.ExampleList.examples, function (example, key) {
+    it('gets a tick size', function () {
 
-    it('should draw a `' + example.name + '`line graph', function () {
-
-      a = executeExampleTest(example, StableFlotr, nodeA);
-      b = executeExampleTest(example, TestFlotr, nodeB);
-
-      expect(b).toImageDiffEqual(a);
+      expect(Flotr.getTickSize).not.toBeUndefined();
+      expect(Flotr.getTickSize(10, 0, 100, 1)).toEqual(10);
+      expect(Flotr.getTickSize(20, 0, 100, 1)).toEqual(5);
+      expect(Flotr.getTickSize(5, 10, 110, 1)).toEqual(20);
+      expect(Flotr.getTickSize(0, 0, 10, 1)).toEqual(Number.POSITIVE_INFINITY);
+      expect(isNaN(Flotr.getTickSize(0, 0, -10, 1))).toBeTruthy();
     });
   });
-
-  function executeExampleTest (example, flotr, node) {
-    Math.seedrandom(example.key);
-    Flotr = flotr;
-    example.callback(node);
-    return node.graph.ctx;
-  }
-
-  function buildNode () {
-    var node = document.createElement('div');
-    document.body.appendChild(node);
-    node.style.width = '320px';
-    node.style.height = '240px';
-    return node;
-  }
-
-  function destroyNode (node) {
-    document.body.removeChild(node);
-  }
 });
