b237147203eae6e7a348da23e87b83d39cc9ccc9
Carl Sutherland <carl@humblesoftware.com>
2011-12-28 15:28:21 -0500

    Disable chart tests.

--- spec/Chart.js
+++ spec/Chart.js
@@ -1,11 +1,13 @@
 describe('Options', function () {
 
+  /*
   var
     lines = TestFlotr.charts.lines;
 
   it ('should find a range.', function () {
     lines.range([[0, 1], [1, 4]]);
   });
+  */
 
 
 });
