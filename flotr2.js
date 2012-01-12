c7190731e11ca4eaef694b9b9855d2b3bb77512c
Carl Sutherland <carl@humblesoftware.com>
2012-01-12 13:57:54 -0500

    Built.

--- flotr2.js
+++ flotr2.js
@@ -2384,6 +2384,7 @@ Graph.prototype = {
     this.mouseUpHandler = _.bind(this.mouseUpHandler, this);
     E.observe(document, 'mouseup', this.mouseUpHandler);
     E.fire(this.el, 'flotr:mousedown', [event, this]);
+    this.ignoreClick = false;
   },
   /**
    * Observes the mouseup event for the document.
@@ -2455,7 +2456,7 @@ Graph.prototype = {
     this._handles = [];
     this.lastMousePos = {pageX: null, pageY: null };
     this.plotOffset = {left: 0, right: 0, top: 0, bottom: 0};
-    this.ignoreClick = false;
+    this.ignoreClick = true;
     this.prevHit = null;
   },
 
