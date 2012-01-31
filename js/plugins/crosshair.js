9e1a8b126dbec0e91a8e605902bb93778f0e0f5f
Carl Sutherland <carl@humblesoftware.com>
2012-01-31 15:04:19 -0500

    Simply check for last mouse position.

--- js/plugins/crosshair.js
+++ js/plugins/crosshair.js
@@ -64,10 +64,9 @@ Flotr.addPlugin('crosshair', {
    * Removes the selection box from the overlay canvas.
    */
   clearCrosshair: function() {
-    if (this.lastMousePos.relX != null)
+    if (this.lastMousePos.relX)
       this.octx.clearRect(this.lastMousePos.relX-0.5, this.plotOffset.top, 1,this.plotHeight+1);
-    
-    if (this.lastMousePos.relY != null)
+    if (this.lastMousePos.relY)
       this.octx.clearRect(this.plotOffset.left, this.lastMousePos.relY-0.5, this.plotWidth+1, 1);    
   }
 });
