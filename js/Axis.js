83d193ebdb99a90222cc333c09a3dd557d91a6f3
Marc Silverman <silverma@adobe.com>
2012-06-23 13:15:24 -0700

    Make it possible to change the font size of the axis labels.  Also make distinct styles for the x and y axis so that they can be modified separately from the style sheet.

--- js/Axis.js
+++ js/Axis.js
@@ -174,7 +174,7 @@ Axis.prototype = {
     this.maxLabel = T.dimensions(
       maxLabel,
       {size:options.fontSize, angle: Flotr.toRad(this.options.labelsAngle)},
-      'font-size:smaller;',
+      'font-size:' + this.options.fontSize + ';',
       'flotr-grid-label'
     );
 
