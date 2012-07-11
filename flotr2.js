9fe1374b2c7cbbe534a4312841adf1385442d32e
Carl Sutherland <carl@humblesoftware.com>
2012-07-11 19:20:33 -0400

    Built.
    Includes fix for getArea and handles.

--- flotr2.js
+++ flotr2.js
@@ -5893,15 +5893,27 @@ Flotr.addPlugin('selection', {
   // TODO This isn't used.  Maybe it belongs in the draw area and fire select event methods?
   getArea: function() {
 
-    var s = this.selection.selection,
+    var
+      s = this.selection.selection,
+      a = this.axes,
       first = s.first,
-      second = s.second;
+      second = s.second,
+      x1, x2, y1, y2;
+
+    x1 = a.x.p2d(s.first.x);
+    x2 = a.x.p2d(s.second.x);
+    y1 = a.y.p2d(s.first.y);
+    y2 = a.y.p2d(s.second.y);
 
     return {
-      x1: Math.min(first.x, second.x),
-      x2: Math.max(first.x, second.x),
-      y1: Math.min(first.y, second.y),
-      y2: Math.max(first.y, second.y)
+      x1 : Math.min(x1, x2),
+      y1 : Math.min(y1, y2),
+      x2 : Math.max(x1, x2),
+      y2 : Math.max(y1, y2),
+      xfirst : x1,
+      xsecond : x2,
+      yfirst : y1,
+      ysecond : y2
     };
   },
 
@@ -5913,25 +5925,11 @@ Flotr.addPlugin('selection', {
    * Fires the 'flotr:select' event when the user made a selection.
    */
   fireSelectEvent: function(name){
-    var a = this.axes,
-        s = this.selection.selection,
-        x1, x2, y1, y2;
-
+    var
+      area = this.selection.getArea();
     name = name || 'select';
-
-    x1 = a.x.p2d(s.first.x);
-    x2 = a.x.p2d(s.second.x);
-    y1 = a.y.p2d(s.first.y);
-    y2 = a.y.p2d(s.second.y);
-
-    E.fire(this.el, 'flotr:'+name, [{
-      x1:Math.min(x1, x2), 
-      y1:Math.min(y1, y2), 
-      x2:Math.max(x1, x2), 
-      y2:Math.max(y1, y2),
-      xfirst:x1, xsecond:x2, yfirst:y1, ysecond:y2,
-      selection : s
-    }, this]);
+    area.selection = this.selection.selection;
+    E.fire(this.el, 'flotr:'+name, [area, this]);
   },
 
   /**
