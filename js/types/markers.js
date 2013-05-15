7023350770191fded045b4f80a4ddf5184cd0a6e
Niklas Nilsson <splitfeed@gmail.com>
2013-05-15 21:02:55 +0300

    Fixed image blurring
    When position becomes a float the image is blurred. By using parseInt we force it to render properly and might even improve performance according to the article.
    
    See http://www.html5rocks.com/en/tutorials/canvas/performance/#toc-avoid-float

--- js/types/markers.js
+++ js/types/markers.js
@@ -127,7 +127,7 @@ Flotr.addType('markers', {
       context.strokeRect(left, top, dim.width, dim.height);
     
     if (isImage(label))
-      context.drawImage(label, left+margin, top+margin);
+      context.drawImage(label, parseInt(left+margin), parseInt(top+margin));
     else
       Flotr.drawText(context, label, left+margin, top+margin, {textBaseline: 'top', textAlign: 'left', size: options.fontSize, color: options.color});
   }
